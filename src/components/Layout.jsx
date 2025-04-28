1ed1220f3819d9d25652213ad8c9397cb4ba16b1
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-28 18:24:35 +0100

    feat: add initial app layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,37 @@
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-template-areas: 
+    'header'
+    'main';
+  min-height: 100vh;
+  background-color: aliceblue;
+`
+
+const Header = styled.div`
+  grid-area: header;
+  padding: 1rem;
+`
+
+const Main = styled.div`
+  grid-area: main;
+  padding: 1rem;
+`
+
+const Layout = () => {
+  return (
+    <Container>
+      <Header>
+        Header
+      </Header>
+      <Main>
+        <Outlet />
+      </Main>
+    </Container>
+  )
+}
+
+export default Layout;
