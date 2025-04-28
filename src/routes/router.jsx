b9103f8bb7c02bc1cbc94225c4ba35ac82eb6621
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-28 18:19:08 +0100

    feat: add initial routes

---
+++ src/routes/router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/Layout";
+import HomePage from "../pages/HomePage";
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '', element: <HomePage /> },
+    ]
+  },
+]);
+
+export default routes;
