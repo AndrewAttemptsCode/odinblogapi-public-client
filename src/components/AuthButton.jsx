c6af1e1d7532ccf21052de9439de52cc47c94b0e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-29 18:42:32 +0100

    feat: add auth button to route to the login page or log out if user is logged in that displays their username

---
+++ src/components/AuthButton.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+const AuthButton = () => {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  }
+
+  return (
+    <div>
+      {user && user.username}
+
+      {user ? (
+        <button onClick={handleLogout}>Logout</button>
+      ) : (
+        <button onClick={() => navigate('/login')}>Login</button>
+      )}
+    </div>
+  );
+};
+
+export default AuthButton;
