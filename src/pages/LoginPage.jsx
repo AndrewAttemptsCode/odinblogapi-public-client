729bc503e3ce8dcd5677f7345d6292c7c5654268
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-29 18:41:18 +0100

    feat: add login page that uses the login form component

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "../components/LoginForm"
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Account Login</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
