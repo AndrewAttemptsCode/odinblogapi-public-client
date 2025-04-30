5ca372a3366863ee916aab050f082cd0053f94ad
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-30 17:07:23 +0100

    feat: add a use effect to check if token exists in localStorage, which means the user is logged in, so it redirects back to root path

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import LoginForm from "../components/LoginForm"
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      navigate('/');
+    }
+  }, [navigate]);
+
   return (
     <div>
       <h1>Account Login</h1>
