f39cb5473dfa78427e844a98f4b037abb557f843
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-01 16:42:00 +0100

    feat: add register account route

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout";
 import BlogPage from "../pages/BlogPage";
 import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 
 const routes = createBrowserRouter([
   {
@@ -10,6 +11,7 @@ const routes = createBrowserRouter([
     children: [
       { path: '', element: <BlogPage /> },
       { path: '/login', element: <LoginPage /> },
+      { path: '/register', element: <RegisterPage /> },
     ]
   },
 ]);
