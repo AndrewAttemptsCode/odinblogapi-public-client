9bc199cd3cb20ada15ac00a136ba53d2ab0b3b7f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-01 18:15:19 +0100

    feat: add conditional check to whether token exists, meaning logged in, so it navigation bounces back to root

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import RegisterForm from "../components/RegisterForm";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      navigate('/');
+    }
+  }, [navigate]);
+
   return (
     <div>
       <h1>Register an Account</h1>
