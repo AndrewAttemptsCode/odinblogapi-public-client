fa8900b3da54c4239be1fd1c89290e0dfd40445f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-03 13:22:04 +0100

    feat: add router link to a posts, post page

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const PostCard = ({ post }) => {
   return (
     <article>
       <p>{post.title}</p>
       <p>{post.author.username}</p>
+      <p>{post.text}</p>
+      <Link to={`/posts/${post.id}`}>Read more...</Link>
     </article>
   );
 }
