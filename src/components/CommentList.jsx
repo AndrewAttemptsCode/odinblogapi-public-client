c1625a92da47236c6ed4e3610c9ab0219d5f176f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-03 16:29:21 +0100

    feat: add component to list related post comments

---
+++ src/components/CommentList.jsx
@@ -0,0 +1,18 @@
+import { formatDate } from "../utils";
+
+const CommentList = ({ postComments }) => {
+  return (
+    <>
+      {postComments.map((comment) => (
+        <div key={comment.id}>
+          <p>{comment.author.username}</p>
+          <p>{formatDate(comment.createdAt)}</p>
+          <p>{comment.text}</p>
+          <hr />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default CommentList;
