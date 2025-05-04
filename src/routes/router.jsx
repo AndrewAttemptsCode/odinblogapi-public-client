c910f628c9defa2bc595036a79e45be7af0190eb
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-04 17:29:59 +0100

    feat: add error element, error handling

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,17 +4,19 @@ import BlogPage from "../pages/BlogPage";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import BlogPostPage from "../pages/BlogPostPage";
+import ErrorPage from "../pages/ErrorPage";
 
 const routes = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '', element: <BlogPage /> },
       { path: '/login', element: <LoginPage /> },
       { path: '/register', element: <RegisterPage /> },
       { path: '/posts/:postId', element: <BlogPostPage /> },
-    ]
+    ],
   },
 ]);
 
