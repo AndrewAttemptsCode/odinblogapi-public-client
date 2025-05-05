7865201b0b9346c909d4a2bd878e63879e7febb8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-05 18:40:20 +0100

    feat: add handleSubmit comment logic

--- src/components/CreateComment.jsx
+++ src/components/CreateComment.jsx
@@ -1,12 +1,40 @@
 import { useState } from "react";
 
-const CreateComment = () => {
+const CreateComment = ({ postId, refreshComments }) => {
   const [commentText, setCommentText] = useState('');
+  const [errors, setErrors] = useState(null);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // TODO: Do this tomorrow, handle the comment POST
-    // /posts/:postId/comments restful route
+    const token = localStorage.getItem('token');
+    try {
+      const response = await fetch(`http://localhost:8080/posts/${postId}/comments`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({ text: commentText }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        if (data.errors) {
+          setErrors(data.errors);
+        }
+      }
+
+      if (response.ok) {
+        setCommentText('');
+        refreshComments();
+        setErrors(null);
+      }
+
+    } catch (error) {
+      console.error('Create comment error:', error);
+      setErrors([{ path: 'text', msg: 'Internal Server Error' }]);
+    }
   }
 
   const handleOnChange = (event) => {
@@ -16,6 +44,8 @@ const CreateComment = () => {
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" name="text" id="text" value={commentText} onChange={handleOnChange} placeholder="Drop a comment..." />
+      <p>{errors?.find(error => error.path === 'text')?.msg}</p>
+      <p>{errors?.find(error => error.path === 'auth')?.msg}</p>
       <button type="submit">Send Message</button>
     </form>
   );
