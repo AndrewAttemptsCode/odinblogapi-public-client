9b6d9242045c5e2d2b8eb3dd39c7e6a4bfc612fd
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-05 18:39:43 +0100

    feat: pass fetchPost to create comment component as prop so that on comment post, it will refresh the comments display. Remove fetchPost from useEffect so that fetchPost could be passed as prop function

--- src/pages/BlogPostPage.jsx
+++ src/pages/BlogPostPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PostItem from "../components/PostItem";
 import CommentList from "../components/CommentList";
@@ -11,57 +11,57 @@ const BlogPostPage = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const response = await fetch(`http://localhost:8080/posts/${postId}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+  const fetchPost = useCallback(async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/posts/${postId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-        const data = await response.json();
+      const data = await response.json();
 
-        if (!response.ok) {
-          const error = new Error('Failed to fetch post from server.');
-          error.status = response.status;
-          error.statusText = response.statusText;
-          throw error;
-        }
-
-        if (!data.post) {
-          const error = new Error('The requested blog post could not be found.');
-          error.status = 404;
-          error.statusText = 'Post Not Found';
-          throw error;
-        }
-
-        setPostDetails({
-          author: data.post.author.username,
-          title: data.post.title,
-          text: data.post.text,
-          createdAt: data.post.createdAt,
-        });
-
-        setPostComments(data.post.comments);
-      } catch (error) {
-        if (error.message === 'Failed to fetch') {
-          setError({
-            status: 503,
-            statusText: 'Service Unavailable',
-            message: 'Could not connect to the server, please try again later.',
-          });
-        } else {
-          setError(error);
-        }
-      } finally {
-        setLoading(false);
+      if (!response.ok) {
+        const error = new Error('Failed to fetch post from server.');
+        error.status = response.status;
+        error.statusText = response.statusText;
+        throw error;
       }
-    };
 
-    fetchPost();
+      if (!data.post) {
+        const error = new Error('The requested blog post could not be found.');
+        error.status = 404;
+        error.statusText = 'Post Not Found';
+        throw error;
+      }
+
+      setPostDetails({
+        author: data.post.author.username,
+        title: data.post.title,
+        text: data.post.text,
+        createdAt: data.post.createdAt,
+      });
+
+      setPostComments(data.post.comments);
+    } catch (error) {
+      if (error.message === 'Failed to fetch') {
+        setError({
+          status: 503,
+          statusText: 'Service Unavailable',
+          message: 'Could not connect to the server, please try again later.',
+        });
+      } else {
+        setError(error);
+      }
+    } finally {
+      setLoading(false);
+    }
   }, [postId]);
+
+  useEffect(() => {
+    fetchPost();
+  }, [fetchPost]);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -75,7 +75,7 @@ const BlogPostPage = () => {
     <section>
       <h1>Blog Post</h1>
       <PostItem postDetails={postDetails} />
-      <CreateComment />
+      <CreateComment postId={postId} refreshComments={fetchPost} />
       <CommentList postComments={postComments} />
     </section>
   );
