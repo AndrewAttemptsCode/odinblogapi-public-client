6b7bdab87e878f9d64f611b2160343cd7e8aefbc
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-06 17:45:04 +0100

    feat: add token as a query param to pass to the admin client side site

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/useAuth";
 
 const AdminPanel = () => {
-const { isAdmin } = useAuth();
+  const { isAdmin } = useAuth();
+
+  const token = localStorage.getItem('token');
 
   return (
     <div>
-      {isAdmin && <Link to={'http://localhost:5174'}>Admin Panel</Link>}
+      {isAdmin && <Link to={`http://localhost:5174/?token=${token}`}>Admin Panel</Link>}
     </div>
   );
 };
