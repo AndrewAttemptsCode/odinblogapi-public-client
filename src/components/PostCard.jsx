cbc0728266bb900a3cf3943d9a7a3b9c1a072d28
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-11 15:24:24 +0100

    feat: add preview first line of post text

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const PostText = styled.p`
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  max-width: 65ch;
+  width: 100%;
+`;
+
 
 const PostCard = ({ post }) => {
   return (
     <article>
       <p>{post.title}</p>
       <p>{post.author.username}</p>
-      <p>{post.text}</p>
+      <PostText>{post.text}</PostText>
       <Link to={`/posts/${post.id}`}>Read more...</Link>
     </article>
   );
