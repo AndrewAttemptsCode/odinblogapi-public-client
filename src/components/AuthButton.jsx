7349910d4d35036ba105c60eb5b6a73f4d48dbfb
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-11 16:44:19 +0100

    styles: add layout and styling to the auth button section

--- src/components/AuthButton.jsx
+++ src/components/AuthButton.jsx
@@ -1,5 +1,31 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/useAuth";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  font-size: 1.1rem;
+  font-weight: bold;
+  color: #111827;
+
+  & p {
+    font-weight: 400;
+  }
+`
+
+const Button = styled.button`
+  padding: 8px 16px;
+  background-color: #3B82F6;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.9;
+  }
+`
 
 const AuthButton = () => {
   const { user, logout } = useAuth();
@@ -11,15 +37,15 @@ const AuthButton = () => {
   }
 
   return (
-    <div>
-      {user && user.username}
+    <Container>
+      <p>{user && user.username}</p>
 
       {user ? (
-        <button onClick={handleLogout}>Logout</button>
+        <Button onClick={handleLogout}>Logout</Button>
       ) : (
-        <button onClick={() => navigate('/login')}>Login</button>
+        <Button onClick={() => navigate('/login')}>Login</Button>
       )}
-    </div>
+    </Container>
   );
 };
 
