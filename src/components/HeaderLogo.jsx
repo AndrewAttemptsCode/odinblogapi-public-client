cd7b9b17144acc619d5deac7031d7f8bfa5ce8d8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-11 16:23:49 +0100

    feat: add logo with link back to homepage

---
+++ src/components/HeaderLogo.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Logo = styled.h1`
+  color: #3B82F6;
+  transition: transform 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  & span {
+    color: #111827;
+  }
+`
+
+const StyledLink = styled(Link)`
+  margin-right: auto;
+  text-decoration: none;
+`
+
+const HeaderLogo = () => {
+  return (
+    <StyledLink to={'/'}>
+      <Logo>B<span>logger</span></Logo>
+    </StyledLink>
+  );
+};
+
+export default HeaderLogo;
