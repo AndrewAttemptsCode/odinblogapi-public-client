f2c6cf5d8df26390620764747ff5f68152b2bdc6
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-11 19:47:26 +0100

    styles: add post section layout control

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,10 +1,34 @@
+import styled from "styled-components";
 import PostList from "../components/PostList";
+
+const TitleSection = styled.section`
+  display: flex;
+  justify-content: center;
+`
+const PostListSection = styled.section`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`
+
+const PageTitle = styled.h1`
+  color: #3B82F6;
+  font-size: 3rem;
+  
+  & span {
+    color: #111827;
+  }
+`
 
 const BlogPage = () => {
   return (
     <section>
-      <h1>Blog Posts</h1>
-      <PostList />
+      <TitleSection>
+      <PageTitle>Blog <span>Posts</span></PageTitle>
+      </TitleSection>
+      <PostListSection>
+        <PostList />
+      </PostListSection>
     </section>
   )
 }
