cd6711bfcef96abee98771a608d2e3981fc410a6
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-12 13:38:09 +0100

    styles: add responsive design control and icons

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,14 +1,41 @@
+import styled from "styled-components";
 import { formatDate } from "../utils";
+import { CalendarDays, User } from "lucide-react";
+
+const Info = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding-bottom: 1rem;
+`
+
+const Item = styled.section`
+  color: #111827;
+  max-width: 75ch;
+  width: 100%;
+
+  & h2 {
+    font-size: 2rem;
+  }
+
+  & p {
+    white-space: pre-wrap;
+  }
+`
 
 const PostItem = ({ postDetails }) => {
   return (
-    <div>
+    <Item>
       <h2>{postDetails.title}</h2>
-      <p>{postDetails.author}</p>
-      <p>{formatDate(postDetails.createdAt)}</p>
+      <Info>
+        <User size={20} color="#3B82F6" />
+        {postDetails.author}
+        <CalendarDays size={20} color="#3B82F6" />
+        {formatDate(postDetails.createdAt)}
+      </Info>
       <p>{postDetails.text}</p>
       <hr />
-    </div>
+    </Item>
   );
 };
 
