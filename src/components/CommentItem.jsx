31a7e8d8a311aa3d576ad62ba43e7c1a91837210
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-12 16:08:01 +0100

    styles: add responsive layout and layout of each comment that retains the user input white spacing formatting

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -1,14 +1,53 @@
+import styled from "styled-components";
 import { formatDate, formatTime } from "../utils";
+import { CalendarDays, Clock7, User } from "lucide-react";
+
+const Item = styled.div`
+  color: #111827;
+
+  & p {
+    white-space: pre-wrap;
+  }
+`
+
+const Info = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+
+  & div {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+  }
+
+  @media (max-width: 450px) {
+    flex-direction: column;
+  }
+`
+
+const Hr = styled.hr`
+  margin: 1rem 0;
+`
 
 const CommentItem = ({ comment }) => {
   return (
-    <div>
-      <p>{comment.author.username}</p>
-      <p>{formatDate(comment.createdAt)}</p>
-      <p>{formatTime(comment.createdAt)}</p>
+    <Item>
+      <Info>
+        <div>
+          <User size={20} color="#3B82F6" />
+          <strong>{comment.author.username}</strong>
+        </div>
+        <div>
+          <Clock7 size={20} color="#3B82F6" />
+          {formatTime(comment.createdAt)}
+          <CalendarDays size={20} color="#3B82F6" />
+          {formatDate(comment.createdAt)}
+        </div>
+      </Info>
       <p>{comment.text}</p>
-      <hr />
-    </div>
+      <Hr />
+    </Item>
   );
 };
 
