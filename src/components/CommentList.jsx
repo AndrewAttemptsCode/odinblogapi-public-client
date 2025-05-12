9315b40c82b66e19498d56c15021afba6f013a91
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-12 16:07:13 +0100

    styles: add responsive layout control and display of total comments on a post

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,12 +1,44 @@
+import styled from "styled-components";
 import CommentItem from "./CommentItem";
+import { MessageSquareText } from "lucide-react";
+
+const Section = styled.section`
+  width: 100%;
+  max-width: 75ch;
+  margin-top: 1rem;
+  color: #111827;
+`
+
+const TitleInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 1rem;
+
+  & div {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    font-size: 20px;
+    font-weight: bold;
+    color: #3B82F6;
+  }
+`
 
 const CommentList = ({ postComments }) => {
   return (
-    <section>
+    <Section>
+      <TitleInfo>
+        <h2>Comments</h2>
+        <div>
+          <MessageSquareText size={20} color="#111827"/>
+          {postComments.length}
+        </div>
+      </TitleInfo>
       {postComments.map((comment) => (
         <CommentItem key={comment.id} comment={comment} />
       ))}
-    </section>
+    </Section>
   );
 };
 
