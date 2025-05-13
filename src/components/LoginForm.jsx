c85706413c2ce2630660fd4ddec0b668a2148431
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-13 14:41:38 +0100

    styles: add responsive design and form styling

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,36 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../contexts/useAuth";
 import { useNavigate, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 300px;
+  color: #111827;
+
+  & input {
+    padding: 5px;
+  }
+
+  & button {
+    border: none;
+    border-radius: 5px;
+    padding: 8px;
+    background-color: #3B82F6;
+    color: #F9FAFB;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.9;
+    }
+  }
+`
+
+const ErrorText = styled.p`
+  color: red;
+`
 
 const LoginForm = () => {
   const { login, errors, clearErrors } = useAuth();
@@ -39,17 +69,17 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleLogin}>
+    <Form onSubmit={handleLogin}>
       <label htmlFor="username">Username:</label>
       <input type="text" name="username" id="username" value={formData.username} onChange={handleOnChange} />
-      <p>{errors?.find(error => error.path === 'username')?.msg}</p>
+      <ErrorText>{errors?.find(error => error.path === 'username')?.msg}</ErrorText>
       <label htmlFor="password">Password:</label>
       <input type="password" name="password" id="password" value={formData.password} onChange={handleOnChange} />
-      <p>{errors?.find(error => error.path === 'password')?.msg}</p>
-      <p>{errors?.find(error => error.path === 'form')?.msg}</p>
+      <ErrorText>{errors?.find(error => error.path === 'password')?.msg}</ErrorText>
+      <ErrorText>{errors?.find(error => error.path === 'form')?.msg}</ErrorText>
       <p>Need an account? <Link to={'/register'}>Register</Link></p>
       <button type="submit" disabled={loading}>{loading ? 'Processing...' : 'Login'}</button>
-    </form>
+    </Form>
   );
 };
 
